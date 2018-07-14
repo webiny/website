bbc73f1896c289e20d364e76f98b5c9bb5a8ba86
sven <sven@webiny.com>
2018-07-14 21:37:58 +0100

    Added lambda services for subscribe and contact us.

---
+++ services/contact-us.js
@@ -0,0 +1,85 @@
+const qs = require('querystring')
+const aws = require('aws-sdk')
+
+const ses = new aws.SES({
+  accessKeyId: 'AKIAIRWHLABZ6QTXYMNA',
+  secretAccesskey: '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',
+  region: 'us-east-1',
+})
+
+// Lambda Handler
+module.exports.handler = function(event, context, callback) {
+  const SUBSCRIBERS_LIST_ID = 'gVp3rBCx9E6GZtVznTEmwg'
+  const ENDPOINT = 'https://mailer.webiny.com/subscribe'
+
+  // Parse the post body
+  const data = qs.parse(event.body)
+
+  // Access variables from body
+  const email = data.email
+  const name = data.name
+  const message = data.message
+
+  // check all fields are defined
+  if (email == undefined || name == undefined || message == undefined) {
+    // send response
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ status: false, msg: 'All fields are required.' }),
+      isBase64Encoded: false,
+      headers: {},
+    })
+  }
+
+  // email regex
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (!re.test(String(email).toLowerCase())) {
+    // send response
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ status: false, msg: 'Invalid email format.' }),
+      isBase64Encoded: false,
+      headers: {},
+    })
+  }
+
+  // send the email
+  var params = {
+    Destination: {
+      ToAddresses: ['sven@webiny.com'],
+    },
+    ReplyToAddresses: [email],
+    Message: {
+      Body: {
+        Text: {
+          Data: message,
+        },
+      },
+      Subject: {
+        Data: name + ' - via Webiny Contact Form',
+      },
+    },
+    Source: 'info@webiny.com',
+  }
+
+  ses.sendEmail(params, (err, data) => {
+    if (err) {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ status: true, msg: 'Thanks' }),
+        isBase64Encoded: false,
+        headers: {},
+      })
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          status: false,
+          msg: 'Oops...a strange error occurred, we are looking into it.',
+        }),
+        isBase64Encoded: false,
+        headers: {},
+      })
+    }
+  })
+}
