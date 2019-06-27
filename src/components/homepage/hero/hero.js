44349ebd556f4ec9d1e12d9e055ab144f71c8985
sven <sven@webiny.com>
2019-06-27 09:57:10 +0100

    Homepage adjustments

--- src/components/homepage/hero/hero.js
+++ src/components/homepage/hero/hero.js
@@ -1,159 +1,206 @@
-import React from 'react';
-import styled from 'react-emotion';
-import {css} from 'emotion';
-import theme from '../../utils/theme';
-import mq from '../../utils/breakpoints';
-import ContentContainer from '../../ui/content-container';
-import Button from '../../ui/button';
-import Video from './video';
+import React from "react";
+import styled from "react-emotion";
+import { css } from "emotion";
+import theme from "../../utils/theme";
+import mq from "../../utils/breakpoints";
+import ContentContainer from "../../ui/content-container";
+import Button from "../../ui/button";
+import Video from "./video";
 
-import heroBg from './assets/hero-bg-2.svg';
-import gitHubLogo from './assets/github-logo.svg';
+import heroBg from "./assets/hero-bg-2.svg";
+import gitHubLogo from "./assets/github-logo.svg";
 //import buttonArrow from './assets/button-arrow.svg';
-import techStackLogos from './assets/tech-stack-logos.svg';
+import techStackLogos from "./assets/tech-stack-logos.svg";
+import bullet from "./assets/bullet.svg";
 
-const heroContainer = css (
-  {
-    display: 'flex',
-    justifyContent: 'space-between',
-    maxWidth: 1300,
-  },
-  mq ({
-    flexDirection: ['column', 'row'],
-    width: ['100%', 1200],
-  })
+const heroContainer = css(
+    {
+        display: "flex",
+        justifyContent: "space-between",
+        maxWidth: 1300
+    },
+    mq({
+        flexDirection: ["column", "row"],
+        width: ["100%", 1200]
+    })
 );
 
-const Hero = styled ('section') (
-  {
-    width: '100%',
-    background: 'url(' + heroBg + ') no-repeat left bottom',
-    backgroundSize: 'cover',
-    color: '#fff',
-    boxSizing: 'border-box',
-    backgroundColor: theme.color.lightGray,
-  },
-  mq ({
-    textAlign: ['center', 'left'],
-    padding: ['100px 20px 25px', '150px 0 25px'],
-    height: ['auto', 'calc(100vh - 100px)'],
-    minHeight: ['auto', 550],
-    maxHeight: ['auto', 750],
-    marginBottom: [0, -50],
-  })
+const Hero = styled("section")(
+    {
+        width: "100%",
+        background: "url(" + heroBg + ") no-repeat left bottom",
+        backgroundSize: "cover",
+        color: "#fff",
+        boxSizing: "border-box",
+        backgroundColor: theme.color.lightGray
+    },
+    mq({
+        textAlign: ["center", "left"],
+        padding: ["100px 20px 25px", "150px 0 25px"],
+        height: ["auto", "calc(100vh - 100px)"],
+        minHeight: ["auto", 800],
+        //maxHeight: ["auto", 750],
+        marginBottom: [0, -50]
+    })
 );
 
-const Title = styled ('h1') (
-  {
-    fontSize: theme.fontSize.h1,
-    fontWeight: theme.fontWeight.semiBold,
-    marginBottom: 40,
-  },
-  mq ({
-    textAlign: ['center', 'left'],
-  })
+const Title = styled("h1")(
+    {
+        fontSize: theme.fontSize.h1,
+        fontWeight: theme.fontWeight.semiBold,
+        marginBottom: 25
+    },
+    mq({
+        textAlign: ["center", "left"]
+    })
 );
 
-const LeftSide = styled ('div') (
-  {},
-  mq ({
-    textAlign: ['center', 'left'],
-    width: ['100%', 420],
-  })
+const LeftSide = styled("div")(
+    {},
+    mq({
+        textAlign: ["center", "left"],
+        width: ["100%", 420]
+    })
 );
 
-const RightSide = styled ('div') (
-  {},
-  mq ({
-    //textAlign: ['center', 'left'],
-    width: ['100%', 675],
-  })
+const RightSide = styled("div")(
+    {},
+    mq({
+        //textAlign: ['center', 'left'],
+        width: ["100%", 675]
+    })
 );
 
-const Buttons = styled ('div') (
-  {
-    display: 'flex',
-    marginBottom: 25,
-  },
-  mq ({
-    justifyContent: ['space-around', 'flex-start'],
-  })
+const Buttons = styled("div")(
+    {
+        display: "flex",
+        marginBottom: 25
+    },
+    mq({
+        justifyContent: ["space-around", "flex-start"]
+    })
 );
 
-const ctaButton = css (
-  {
-    position: 'relative',
-    marginTop: 35,
-  },
-  mq ({
-    marginRight: [0, '25px !important'],
-    width: ['150px !important', 'auto !important'],
-  })
+const ctaButton = css(
+    {
+        position: "relative",
+        marginTop: 35
+    },
+    mq({
+        marginRight: [0, "25px !important"],
+        width: ["150px !important", "auto !important"]
+    })
 );
 
-const SubText = styled ('p') (
-  {
-    fontSize: theme.fontSize.subText,
-    fontWeight: theme.fontWeight.light,
-    lineHeight: '25px',
-    marginBottom: 35,
-  },
-  mq ({
-    textAlign: ['center', 'left'],
-  })
+const SubText = styled("p")(
+    {
+        fontSize: theme.fontSize.subText,
+        fontWeight: theme.fontWeight.light,
+        lineHeight: "25px",
+        marginBottom: 35
+    },
+    mq({
+        textAlign: ["center", "left"]
+    })
 );
 
-const Link = styled ('a') ({
-  fontSize: 14,
-  color: '#FFFFFF',
-  textAlign: 'left',
-  fontWeight: theme.fontWeight.regular,
-  margin: '15px auto 0 auto',
-  display: 'block',
-  textDecoration: 'none',
-  position: 'relative',
-  paddingLeft: 20,
-  '&:hover': {
-    textDecoration: 'underline',
-  },
+const Link = styled("a")({
+    fontSize: 14,
+    color: "#FFFFFF",
+    textAlign: "left",
+    fontWeight: theme.fontWeight.regular,
+    margin: "15px auto 0 auto",
+    display: "block",
+    textDecoration: "none",
+    position: "relative",
+    paddingLeft: 20,
+    "&:hover": {
+        textDecoration: "underline"
+    }
 });
 
-const GitHubLogo = styled ('img') ({
-  position: 'absolute',
-  left: 0,
-  top: 4,
+const GitHubLogo = styled("img")({
+    position: "absolute",
+    left: 0,
+    top: 4
 });
 
-export default ({children, ...props}) => (
-  <Hero {...props}>
-    <ContentContainer className={heroContainer}>
-      <LeftSide>
-        <Title>
-          Build more than websites
-        </Title>
-        <SubText>
-          The way how we build, deploy and operate the web is evolving.
-          Webiny is a developer-friendly serverless CMS powered by Node and React with a headless GraphQL API.
-        </SubText>
-        <img src={techStackLogos} />
-        <Buttons>
-          <Button className={ctaButton} link="/pricing" type="dark">
-            Get Started
-          </Button>
-          <Button className={ctaButton} link="/contact-us" type="outline">
-            Request demo
-          </Button>
-        </Buttons>
+const List = styled("ul")(
+    {
+        boxSizing: "border-box",
+        padding: 0,
+        width: "100%",
+        marginBottom: 25,
+        display: "flex",
+        flexWrap: "wrap"
+    },
+    mq({
+        justifyContent: ["center", "space-between"]
+    })
+);
 
-        <Link href="https://github.com/Webiny/webiny-js">
-          <GitHubLogo src={gitHubLogo} />
-          View Webiny on GitHub
-        </Link>
+const ListItem = styled("li")({
+    listStyle: "none",
+    textAlign: "left",
+    marginBottom: 15,
+    marginRight: 25,
+    background: "url(" + bullet + ") no-repeat left 2px",
+    paddingLeft: 30,
+    "&:last-child": {
+        marginBottom: 0
+    },
+    a: {
+        color: theme.color.secondaryDark
+    }
+});
 
-      </LeftSide>
-      <RightSide>
-        <Video />
-      </RightSide>
-    </ContentContainer>
-  </Hero>
+const Bold = styled("span")({
+    fontWeight: theme.fontWeight.bold
+});
+
+export default ({ children, ...props }) => (
+    <Hero {...props}>
+        <ContentContainer className={heroContainer}>
+            <LeftSide>
+                <Title>CMS for Serverless Web Development</Title>
+
+                <SubText>
+                    The way how we build, deploy and operate the web is evolving. Webiny is a
+                    developer-friendly serverless CMS powered by Node and React with a headless
+                    GraphQL API.
+                </SubText>
+
+                <List>
+                    <ListItem>
+                        <Bold>Serverless</Bold>
+                    </ListItem>
+                    <ListItem>
+                        <Bold>Headless</Bold>
+                    </ListItem>
+                    <ListItem>
+                        <Bold>Open Source</Bold>
+                    </ListItem>
+                </List>
+
+                <img src={techStackLogos} />
+
+                <Buttons>
+                    <Button className={ctaButton} link="/pricing" type="dark">
+                        Get Started
+                    </Button>
+                    <Button className={ctaButton} link="/contact-us" type="outline">
+                        Request demo
+                    </Button>
+                </Buttons>
+
+                <Link href="https://github.com/Webiny/webiny-js">
+                    <GitHubLogo src={gitHubLogo} />
+                    View Webiny on GitHub
+                </Link>
+            </LeftSide>
+            <RightSide>
+                <Video />
+            </RightSide>
+        </ContentContainer>
+    </Hero>
 );
--- src/components/homepage/what-can-you-do/what-can-you-do.js
+++ src/components/homepage/what-can-you-do/what-can-you-do.js
@@ -1,241 +1,238 @@
-import React from 'react';
-import styled from 'react-emotion';
-import {css} from 'emotion';
-import theme from '../../utils/theme';
-import mq from '../../utils/breakpoints';
-import ContentContainer from '../../ui/content-container';
+import React from "react";
+import styled from "react-emotion";
+import { css } from "emotion";
+import theme from "../../utils/theme";
+import mq from "../../utils/breakpoints";
+import ContentContainer from "../../ui/content-container";
 
-import lambdaLogo from './assets/lambda-logo.svg';
+import lambdaLogo from "./assets/lambda-logo.svg";
 
-const sectionStyle = css (
-  {
-    backgroundColor: theme.color.lightGray,
-    paddingBottom: 25,
-  },
-  mq ({
-    paddingTop: [50, 0],
-    marginTop: [0, 50],
-  })
+const sectionStyle = css(
+    {
+        backgroundColor: theme.color.lightGray,
+        paddingBottom: 25
+    },
+    mq({
+        paddingTop: [50, 0],
+        marginTop: [0, 50]
+    })
 );
 
-const SubTitle = styled ('h2') ({
-  fontSize: theme.fontSize.h2,
-  fontWeight: theme.fontWeight.semiBold,
-  color: theme.color.black,
-  textAlign: 'center',
-  paddingTop: 50,
-  marginTop: 25,
-  marginBottom: 10,
-});
-
-const SubText = styled ('p') (
-  {
-    fontSize: theme.fontSize.h3,
-    fontWeight: theme.fontWeight.regular,
+const SubTitle = styled("h2")({
+    fontSize: theme.fontSize.h2,
+    fontWeight: theme.fontWeight.semiBold,
     color: theme.color.black,
-    textAlign: 'center',
-    margin: '0 20%',
-    lineHeight: '150%',
-    maxWidth: 680,
-  },
-  mq ({
-    margin: ['20px', '0 auto 25px auto'],
-  })
+    textAlign: "center",
+    paddingTop: 50,
+    marginTop: 25,
+    marginBottom: 10
+});
+
+const SubText = styled("p")(
+    {
+        fontSize: theme.fontSize.h3,
+        fontWeight: theme.fontWeight.regular,
+        color: theme.color.black,
+        textAlign: "center",
+        margin: "0 20%",
+        lineHeight: "150%",
+        maxWidth: 680
+    },
+    mq({
+        margin: ["20px", "0 auto 25px auto"]
+    })
 );
 
-const Grid = styled ('div') (
-  {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    boxSizing: 'border-box',
+const Grid = styled("div")(
+    {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        boxSizing: "border-box",
+        marginBottom: 0,
+        borderRadius: 2
+    },
+    mq({
+        flexDirection: ["column", "row"],
+        marginLeft: [0, 0],
+        marginRight: [0, 0],
+        padding: ["0 25px", 50],
+        border: ["none", "1px solid " + theme.color.darkGray]
+    })
+);
+
+const Cell = styled("div")(
+    {
+        backgroundColor: theme.color.white,
+        boxShadow: "rgba(46, 41, 51, 0.08) 2px 2px 2px, rgba(71, 63, 79, 0.08) 2px 2px 4px",
+        borderRadius: 2,
+        padding: "0 15px 5px 15px",
+        alignItems: "center",
+        boxSizing: "border-box",
+        textAlign: "center",
+        flexBasis: "100%",
+        //borderBottom: '5px solid ' + theme.color.primaryDark,
+        marginRight: 40,
+        display: "flex",
+        flexDirection: "row",
+        "&:last-child": {
+            marginRight: "0 !important"
+        }
+    },
+    mq({
+        width: ["100%", 200]
+    })
+);
+
+const Number = styled("h3")({
+    fontSize: 84,
+    width: 100,
+    lineHeight: "100px",
     marginBottom: 0,
-    borderRadius: 2,
-  },
-  mq ({
-    flexDirection: ['column', 'row'],
-    marginLeft: [0, 0],
-    marginRight: [0, 0],
-    padding: ['0 25px', 50],
-    border: ['none', '1px solid ' + theme.color.darkGray],
-  })
-);
-
-const Cell = styled ('div') (
-  {
-    backgroundColor: theme.color.white,
-    boxShadow: 'rgba(46, 41, 51, 0.08) 2px 2px 2px, rgba(71, 63, 79, 0.08) 2px 2px 4px',
-    borderRadius: 2,
-    padding: '0 25px 10px 25px',
-    alignItems: 'center',
-    boxSizing: 'border-box',
-    textAlign: 'center',
-    flexBasis: '100%',
-    //borderBottom: '5px solid ' + theme.color.primaryDark,
-    marginRight: 40,
-    display: 'flex',
-    flexDirection: 'row',
-    '&:last-child': {
-      marginRight: '0 !important',
-    },
-  },
-  mq ({
-    width: ['100%', 300],
-  })
-);
-
-const Number = styled ('h3') ({
-  fontSize: 96,
-  width: 100,
-  lineHeight: '100px',
-  marginBottom: 0,
-  marginTop: 0,
-  textAlign: 'left',
-  marginLeft: 10,
+    marginTop: 0,
+    textAlign: "left",
+    marginLeft: 10
 });
 
-const Text = styled ('p') ({
-  fontSize: 28,
-  width: 200,
-  textAlign: 'left',
-  fontWeight: theme.fontWeight.semiBold,
-  marginTop: 5,
-  marginBottom: 0,
-  '.orange': {
-    color: theme.color.primaryDark,
-  },
-  '&.smaller': {
-    fontSize: 18,
-    textAlign: 'center',
-    width: '100%',
-    padding: '10px',
-  },
+const Text = styled("p")({
+    fontSize: 24,
+    width: 200,
+    textAlign: "left",
+    fontWeight: theme.fontWeight.semiBold,
+    marginTop: 5,
+    marginBottom: 0,
+    ".orange": {
+        color: theme.color.primaryDark
+    },
+    "&.smaller": {
+        fontSize: 18,
+        textAlign: "center",
+        width: "100%",
+        padding: "10px"
+    }
 });
 
-const InsideServerless = styled ('div') (
-  {
-    textAlign: 'center',
-    alignItems: 'center',
-    display: 'flex',
-    boxSizing: 'border-box',
-    [Cell]: {
-      padding: '50px 25px',
-      backgroundColor: theme.color.footerBg,
-      color: theme.color.white,
+const InsideServerless = styled("div")(
+    {
+        textAlign: "center",
+        alignItems: "center",
+        display: "flex",
+        boxSizing: "border-box",
+        [Cell]: {
+            padding: "50px 25px",
+            backgroundColor: theme.color.footerBg,
+            color: theme.color.white
+        },
+        h4: {
+            fontWeight: theme.fontWeight.semiBold,
+            textAlign: "center",
+            textTransform: "uppercase",
+            width: "100%",
+            marginTop: 0
+        },
+        ".powered-by": {
+            fontSize: 12,
+            color: theme.color.white
+        },
+        ".aws-lambda": {
+            fontSize: 16,
+            color: theme.color.white,
+            fontWeight: theme.fontWeight.semiBold,
+            position: "relative",
+            marginRight: 30,
+            img: {
+                position: "absolute",
+                right: -30,
+                bottom: 0
+            }
+        }
     },
-    h4: {
-      fontWeight: theme.fontWeight.semiBold,
-      textAlign: 'center',
-      textTransform: 'uppercase',
-      width: '100%',
-      marginTop: 0,
-    },
-    '.powered-by': {
-      fontSize: 12,
-      color: theme.color.white,
-    },
-    '.aws-lambda': {
-      fontSize: 16,
-      color: theme.color.white,
-      fontWeight: theme.fontWeight.semiBold,
-      position: 'relative',
-      marginRight: 30,
-      img: {
-        position: 'absolute',
-        right: -30,
-        bottom: 0,
-      },
-    },
-  },
-  mq ({
-    width: ['100%', '550px'],
-    margin: [0, '50px auto'],
-    padding: [25, 0],
-    [Cell]: {
-      flexDirection: ['column', 'column'],
-    },
-  })
+    mq({
+        width: ["100%", "550px"],
+        margin: [0, "50px auto"],
+        padding: [25, 0],
+        [Cell]: {
+            flexDirection: ["column", "column"]
+        }
+    })
 );
 
 class WhatCanYouDo extends React.Component {
-  render () {
-    return (
-      <section {...this.props} className={sectionStyle}>
-        <SubTitle>What you can do with Webiny</SubTitle>
-        <SubText>
-          Because the technology powering our CMS is designed to handle all kinds of challenges and uses-cases, you can use it to build much more than just websites.
-        </SubText>
+    render() {
+        return (
+            <section {...this.props} className={sectionStyle}>
+                <SubTitle>What you can do with Webiny</SubTitle>
+                <SubText>
+                    Because the technology powering our CMS is designed to handle all kinds of
+                    challenges and uses-cases, you can use it to build much more than just websites.
+                </SubText>
 
-        <ContentContainer>
-          <Grid>
-            <Cell
-              data-aos="fade-left"
-              data-aos-delay="100"
-              data-aos-offset="200"
-            >
-              <Number>1.</Number>
-              <Text>Create <span className="orange">websites</span></Text>
-            </Cell>
-            <Cell
-              data-aos="fade-left"
-              data-aos-delay="300"
-              data-aos-offset="200"
-            >
-              <Number>2.</Number>
-              <Text>Build <br /><span className="orange">APIs</span></Text>
-            </Cell>
-            <Cell
-              data-aos="fade-left"
-              data-aos-delay="600"
-              data-aos-offset="200"
-            >
-              <Number>3.</Number>
-              <Text>
-                Make
-                {' '}
-                <span className="orange">web</span>
-                <br />
-                <span className="orange">applications</span>
-              </Text>
-            </Cell>
-          </Grid>
-          <InsideServerless>
-            <Cell>
-              <h4>
-                Inside the serverless environment
-              </h4>
-              <div className={'powered-by'}>Powered by</div>
-              <div className={'aws-lambda'}>
-                AWS Lambda
-                <img src={lambdaLogo} height={24} />
-              </div>
-            </Cell>
-          </InsideServerless>
-          <SubText>
-            Webiny provides you with
-          </SubText>
-          <Grid className="smaller">
-            <Cell>
-
-              <Text className={'smaller'}>
-                Technology and components to create web projects
-              </Text>
-            </Cell>
-            <Cell>
-              <Text className={'smaller'}>
-                Managed and scalable serverless platform to run your code on
-              </Text>
-            </Cell>
-            <Cell>
-              <Text className={'smaller'}>
-                Tools to automate project creation and deployment
-              </Text>
-            </Cell>
-          </Grid>
-        </ContentContainer>
-      </section>
-    );
-  }
+                <ContentContainer>
+                    <Grid>
+                        <Cell data-aos="fade-left" data-aos-delay="100" data-aos-offset="200">
+                            <Number>1.</Number>
+                            <Text>
+                                Create <span className="orange">websites</span>
+                            </Text>
+                        </Cell>
+                        <Cell data-aos="fade-left" data-aos-delay="300" data-aos-offset="200">
+                            <Number>2.</Number>
+                            <Text>
+                                Build <br />
+                                <span className="orange">APIs</span>
+                            </Text>
+                        </Cell>
+                        <Cell data-aos="fade-left" data-aos-delay="600" data-aos-offset="200">
+                            <Number>3.</Number>
+                            <Text>
+                                Make <span className="orange">web</span>
+                                <br />
+                                <span className="orange">applications</span>
+                            </Text>
+                        </Cell>
+                        <Cell data-aos="fade-left" data-aos-delay="600" data-aos-offset="200">
+                            <Number>4.</Number>
+                            <Text>
+                                Construct <br /> <span className="orange">microservices</span>
+                            </Text>
+                        </Cell>
+                    </Grid>
+                    <InsideServerless>
+                        <Cell>
+                            <h4>Inside the serverless environment</h4>
+                            <div className={"powered-by"}>Powered by</div>
+                            <div className={"aws-lambda"}>
+                                AWS Lambda
+                                <img src={lambdaLogo} height={24} />
+                            </div>
+                        </Cell>
+                    </InsideServerless>
+                    <SubText>Webiny provides you with</SubText>
+                    <Grid className="smaller">
+                        <Cell>
+                            <Text className={"smaller"}>
+                                <span className="orange">Technology</span> and{" "}
+                                <span className="orange">components</span> to create web projects
+                            </Text>
+                        </Cell>
+                        <Cell>
+                            <Text className={"smaller"}>
+                                Managed and scalable{" "}
+                                <span className="orange">serverless platform</span> to run your code
+                                on
+                            </Text>
+                        </Cell>
+                        <Cell>
+                            <Text className={"smaller"}>
+                                Tools to automate <span className="orange">project creation</span>{" "}
+                                and <span className="orange">deployment</span>
+                            </Text>
+                        </Cell>
+                    </Grid>
+                </ContentContainer>
+            </section>
+        );
+    }
 }
 
 export default WhatCanYouDo;
