5f6b827672c03b47a7ccd878da68d8ee87e04488
SvenAlHamad <sven@webiny.com>
2019-08-22 14:55:37 +0100

    Serverless hero minor css fix

--- src/components/why-serverless/hero.js
+++ src/components/why-serverless/hero.js
@@ -1,26 +1,32 @@
-import React from "react";
-import { css } from "emotion";
-import Title from "../ui/layout/title";
-import Subtitle from "../ui/layout/subtitle";
-import Header from "../ui/layout/header";
+import React from 'react';
+import {css} from 'emotion';
+import mq from '../utils/breakpoints';
+import Title from '../ui/layout/title';
+import Subtitle from '../ui/layout/subtitle';
+import Header from '../ui/layout/header';
 
-import headerBg from "./assets/hero-bg.svg";
+import headerBg from './assets/hero-bg.svg';
 
-const heroStyle = css({
-    paddingBottom: "200px !important"
-});
+const heroStyle = css (
+  {
+    paddingBottom: '200px !important',
+  },
+  mq ({
+    minHeight: ['auto', 'auto', 650],
+  })
+);
 
 const Hero = () => (
-    <React.Fragment>
-        <Header bg={headerBg} className={heroStyle}>
-            <Title>Why serverless?</Title>
-            <Subtitle>
-                Serverless is making big changes to how we operate, deploy, architect and run
-                applications. There are many benefits that serverless brings, the ones below are
-                just a few.
-            </Subtitle>
-        </Header>
-    </React.Fragment>
+  <React.Fragment>
+    <Header bg={headerBg} className={heroStyle}>
+      <Title>Why serverless?</Title>
+      <Subtitle>
+        Serverless is making big changes to how we operate, deploy, architect and run
+        applications. There are many benefits that serverless brings, the ones below are
+        just a few.
+      </Subtitle>
+    </Header>
+  </React.Fragment>
 );
 
 export default Hero;
