f97b74845e7869e0f9bbb1170d41c4a6447b42a2
SvenAlHamad <sven@webiny.com>
2020-01-13 17:43:33 +0000

    SEO update

--- src/pages/guides/serverless-guide.js
+++ src/pages/guides/serverless-guide.js
@@ -7,7 +7,7 @@ export default ({children, ...props}) => (
   <BaseLayout
     fixedHeader={true}
     title="Guide to Serverless"
-    description="Learn what serverless is, what are the main products, terms and the basic know-how."
+    description="Learn about serverless. What it is, what are the main products, terms and basic know-how."
   >
     <Serverless />
   </BaseLayout>
