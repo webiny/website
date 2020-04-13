9ab8cf08af357b7995b5c40451ac5e4484d43cff
Ashutosh <bhardwajashu96@hotmail.com>
2020-04-13 13:57:24 +0530

    Add `Hero` component to agency page

--- src/pages/agency.js
+++ src/pages/agency.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import BaseLayout from '../layouts/base';
+import Hero from '../components/agency/hero/hero'
 
 class AgencyPage extends React.Component {
   render () {
     return (
       <BaseLayout title="Webiny - How Agencies can benefit">
+        <Hero/>
       </BaseLayout>
     );
   }
