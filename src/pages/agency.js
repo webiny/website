93bc008045f13205e3154583f4513579d3336a20
Ashutosh <bhardwajashu96@hotmail.com>
2020-04-13 15:11:39 +0530

    Add WhatIsWebiny to agency page

--- src/pages/agency.js
+++ src/pages/agency.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import BaseLayout from '../layouts/base';
 import Hero from '../components/agency/hero/hero'
+import WhatIsWebiny from '../components/agency/what-is-webiny/what-is-webiny';
 
 class AgencyPage extends React.Component {
   render () {
     return (
       <BaseLayout title="Webiny - How Agencies can benefit">
         <Hero/>
+        <WhatIsWebiny />
       </BaseLayout>
     );
   }
