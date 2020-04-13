9a812cf676580986128e7f0fc15f63a8707e8536
Ashutosh <bhardwajashu96@hotmail.com>
2020-04-13 13:06:39 +0530

    Add `agency` page

---
+++ src/pages/agency.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import BaseLayout from '../layouts/base';
+
+class AgencyPage extends React.Component {
+  render () {
+    return (
+      <BaseLayout title="Webiny - How Agencies can benefit">
+      </BaseLayout>
+    );
+  }
+}
+
+export default AgencyPage;
