885c4d48a7b0192b1eb0e350a2c71426a70a9cbf
Ashutosh <bhardwajashu96@hotmail.com>
2020-04-13 16:25:58 +0530

    Add WhyServerless section to agency page

--- src/pages/agency.js
+++ src/pages/agency.js
@@ -3,6 +3,7 @@ import React from 'react';
 import BaseLayout from '../layouts/base';
 import Hero from '../components/agency/hero/hero'
 import WhatIsWebiny from '../components/agency/what-is-webiny/what-is-webiny';
+import WhyServerless from '../components/agency/why-serverless/why-serverless';
 
 class AgencyPage extends React.Component {
   render () {
@@ -10,6 +11,7 @@ class AgencyPage extends React.Component {
       <BaseLayout title="Webiny - How Agencies can benefit">
         <Hero/>
         <WhatIsWebiny />
+        <WhyServerless />
       </BaseLayout>
     );
   }
