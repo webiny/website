d94ac8395c15d5cf7d83af6c1a27821cde0749b8
Ashutosh <bhardwajashu96@hotmail.com>
2020-04-14 15:30:36 +0530

    Add `WebinyFeatures` to agency page

--- src/pages/agency.js
+++ src/pages/agency.js
@@ -5,6 +5,7 @@ import Hero from '../components/agency/hero/hero'
 import WhatIsWebiny from '../components/agency/what-is-webiny/what-is-webiny';
 import WhyServerless from '../components/agency/why-serverless/why-serverless';
 import ReadyMadeApps from '../components/agency/ready-made-apps/ready-made-apps';
+import WebinyFeatures from '../components/agency/webiny-features/webiny-features';
 
 class AgencyPage extends React.Component {
   render () {
@@ -14,6 +15,7 @@ class AgencyPage extends React.Component {
         <WhatIsWebiny />
         <WhyServerless />
         <ReadyMadeApps />
+        <WebinyFeatures />
       </BaseLayout>
     );
   }
