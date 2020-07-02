4e112e785fb70b432fcb0c5c1d89eda61a6e0db2
Ashutosh <bhardwajashu96@hotmail.com>
2020-07-02 13:45:20 +0530

    feat: update index page with new homepage content
    add few more colors to theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,35 @@
 import React from 'react';
 
 import BaseLayout from '../layouts/base';
-import Hero from '../components/homepage/hero/hero';
-import WhyWebiny from '../components/homepage/why-webiny/why-webiny';
-import Components from '../components/homepage/components/components';
-import LearnExplore from '../components/homepage/learn-explore/learn-explore';
-import MoreThanCms from '../components/homepage/more-than-cms/more-than-cms';
-import ServerlessBenefits
-  from '../components/homepage/serverless-benefits/serverless-benefits';
-import Testimonials from '../components/homepage/testimonials/testimonials';
+import Hero from '../components/homepage/hero-new/hero';
+import ServerlessEasy from '../components/homepage/serverless-easy/serverless-easy';
+
+import GoServerless from '../components/homepage/go-serverless/go-serverless';
+import ReadyMadeApps from '../components/agency/ready-made-apps/ready-made-apps';
+import ServerlessDIY from '../components/homepage/serverless-diy/serverless-diy';
+import WhyWebinyWhyServerless from '../components/homepage/why-serverless/why-serverless';
+import WhyIsWebiny from '../components/homepage/what-is-webiny/what-is-webiny';
+import ClientsLogo from '../components/homepage/clients-logo/clients-logo';
 
 class IndexPage extends React.Component {
-  render () {
-    return (
-      <BaseLayout
-        title="Webiny - Serverless CMS powered by GraphQL and React"
-        description="Webiny is a modern open source content management designed to run inside a serverless environment.  Webiny is powered by React, Node and GraphQL."
-        fixedHeader={true}
-      >
-        <Hero />
-        <WhyWebiny />
-        <MoreThanCms />
-        <Components />
-        <ServerlessBenefits />
-        <Testimonials />
-        <LearnExplore />
-      </BaseLayout>
-    );
-  }
+    render() {
+        return (
+            <BaseLayout
+                title='Webiny - Serverless CMS powered by GraphQL and React'
+                description='Webiny is a modern open source content management designed to run inside a serverless environment.  Webiny is powered by React, Node and GraphQL.'
+                fixedHeader={false}
+            >
+                <Hero/>
+                <ClientsLogo/>
+                <WhyIsWebiny/>
+                <WhyWebinyWhyServerless/>
+                <ServerlessDIY/>
+                <ReadyMadeApps/>
+                <ServerlessEasy/>
+                <GoServerless/>
+            </BaseLayout>
+        );
+    }
 }
 
 export default IndexPage;
