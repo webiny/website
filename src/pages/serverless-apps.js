a9da063b68f7cdadca3b88555e04be65ddf21564
sven <sven@webiny.com>
2020-07-22 14:16:21 +0200

    Added new apps page to the main menu and updated the route.

--- src/pages/serverless-apps.js
+++ src/pages/serverless-apps.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import BaseLayout from "../../layouts/base";
-import GoServerless from "../../components/homepage/go-serverless/go-serverless";
-import WebinyApps from "../../components/serverless-app/webiny-apps/webiny-apps";
+import BaseLayout from "../layouts/base";
+import GoServerless from "../components/homepage/go-serverless/go-serverless";
+import WebinyApps from "../components/serverless-app/webiny-apps/webiny-apps";
 
 const WebinyAppsPage = () => {
     useEffect(() => {
@@ -12,7 +12,7 @@ const WebinyAppsPage = () => {
 
     return (
         <BaseLayout
-            title="The easiest way to adopt serverless | Webiny"
+            title="Open-source serverless applications | Webiny"
             description="Webiny is an open-source framework for building serverless applications. Webiny is powered by React, Node and GraphQL."
             fixedHeader={false}
         >
