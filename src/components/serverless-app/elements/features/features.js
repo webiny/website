23e76cedc4fbbb4a53df6e81c9477ae3cf3fa58e
Ashutosh <bhardwajashu96@hotmail.com>
2020-07-30 09:51:23 +0530

    feat: add `Features` section for serverless apps
    serverless apps

---
+++ src/components/serverless-app/elements/features/features.js
@@ -0,0 +1,199 @@
+import React, { Fragment } from "react";
+import { css } from "emotion";
+import styled from "react-emotion";
+
+import theme from "../../../utils/theme";
+import mq from "../../../utils/breakpoints";
+import ContentContainer from "../../../ui/content-container";
+import VideoBox from "../../../ui/layout/video-box";
+import Button from "../../../ui/button";
+
+// assets
+import TitleBg from "./assets/features-rect-bg.svg";
+import bg from "./assets/gray-rect.svg";
+
+const FeatureWrapper = styled("div")(
+    {
+        display: "flex",
+        justifyContent: "space-between",
+        boxSizing: "border-box",
+        alignItems: "center",
+
+        "& .text-content": {
+            boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+            "& h3": {
+                margin: "0px 0px 28px",
+
+                fontSize: theme.fontSize["4xl"],
+                color: theme.color.black,
+                fontWeight: theme.fontWeight.bold,
+
+                "& span": {
+                    color: theme.color.purple
+                }
+            },
+            "& p": {
+                width: "100%",
+                maxWidth: 388,
+                fontSize: theme.fontSize.lg,
+                fontWeight: theme.fontWeight.regular,
+                color: theme.color.black,
+                lineHeight: "26px",
+                marginBottom: 16
+            },
+            "& .buttonWrapper": {
+                width: "100%",
+                marginTop: 24
+            }
+        },
+
+        "& .media-content": {
+            boxSizing: "border-box"
+        }
+    },
+    mq({
+        "&.img-left": {
+            flexDirection: ["column", "row"],
+            width: "100%",
+            "& .text-content": {
+                paddingLeft: [0, 60]
+            },
+            "& .media-content": {
+                marginBottom: [60, 0]
+            }
+        },
+        "&.img-right": {
+            flexDirection: ["column-reverse", "row-reverse"],
+            width: "100%",
+
+            "& .media-content": {
+                marginTop: [60, 0]
+            }
+        },
+
+        marginLeft: [0, 0],
+        marginRight: [0, 0],
+        padding: [0, "85px 0"],
+
+        "& .text-content": {
+            width: ["100%", "50%"],
+
+            "& .buttonWrapper": {
+                maxWidth: ["100%", 217]
+            }
+        },
+        "& .media-content": {
+            width: ["100%", "50%"],
+            "& .img": {
+                width: ["100%"]
+            }
+        }
+    })
+);
+
+const Section = styled("section")(
+    {
+        backgroundColor: theme.color.white,
+        position: "relative",
+        zIndex: 0,
+        padding: 0
+    },
+    mq({})
+);
+
+const SectionWithBg = styled("section")(
+    props => ({
+        backgroundColor: theme.color.white,
+        position: "relative",
+        zIndex: 0,
+        backgroundImage: props.background ? "url(" + bg + ")" : "",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "bottom"
+    }),
+    mq({
+        backgroundSize: ["cover", "auto", "100%"],
+        padding: ["0px 15px 110px", "0px 15px"],
+        "@media (min-width: 1460px)": {
+            backgroundPosition: "bottom",
+            paddingTop: 0
+        }
+    })
+);
+
+export const Title = styled("h1")(
+    {
+        position: "relative",
+        maxWidth: 900,
+        fontSize: theme.fontSize["5xl"],
+        fontWeight: theme.fontWeight.bold,
+        color: theme.color.black,
+        textAlign: "center",
+        lineHeight: "48px",
+        marginBottom: 70
+    },
+    mq({
+        marginLeft: [0, "auto"],
+        marginRight: [0, "auto"]
+    })
+);
+
+export const TitleHighlight = styled("span")({
+    backgroundImage: "url(" + TitleBg + ")",
+    backgroundPositionX: "right",
+    backgroundPositionY: "bottom",
+    backgroundRepeat: "no-repeat"
+});
+
+const containerClass = css(
+    {
+        flexDirection: "column",
+        maxWidth: "1200"
+    },
+    mq({
+        width: ["100%", "100%"],
+        display: ["block", "flex"],
+        justifyContent: ["center", "space-between"],
+        padding: ["100px 0px 0px", "0px 0px 0px"]
+    })
+);
+
+const FeaturesSectionTitle = () => (
+    <Section>
+        <ContentContainer className={containerClass}>
+            <Title>
+                <TitleHighlight>Features</TitleHighlight>
+            </Title>
+        </ContentContainer>
+    </Section>
+);
+
+const FeaturesSection = ({ features }) => (
+    <Fragment>
+        {features.map(feature => (
+            <SectionWithBg background={feature.background}>
+                <ContentContainer className={containerClass}>
+                    <FeatureWrapper className={feature.imageOnLeft ? "img-left" : "img-right"}>
+                        <div className={"media-content"}>
+                            {feature.videoFile && <VideoBox file={feature.videoFile} />}
+                            {typeof feature.renderImg === "function" && feature.renderImg()}
+                        </div>
+                        <div className={"text-content"}>
+                            {typeof feature.renderTitle === "function" && feature.renderTitle()}
+                            {typeof feature.renderBody === "function" && feature.renderBody()}
+
+                            <div className={"buttonWrapper"}>
+                                <Button type={"secondary"} link={feature.actionLink}>
+                                    {feature.actionLabel}
+                                </Button>
+                            </div>
+                        </div>
+                    </FeatureWrapper>
+                </ContentContainer>
+            </SectionWithBg>
+        ))}
+    </Fragment>
+);
+
+export { FeaturesSection, FeaturesSectionTitle };
