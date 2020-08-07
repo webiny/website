aebf71354d6d4a5f566c4e06458186dc285cca42
Ashutosh <bhardwajashu96@hotmail.com>
2020-08-07 12:46:36 +0530

    feat: update websites page
    solutions page

--- src/pages/use-case/serverless-websites.js
+++ src/pages/use-case/serverless-websites.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import BaseLayout from "../../layouts/base";
-import Websites from "../../components/use-case/websites";
+import Websites from "../../components/use-case/websites/websites";
 
-class Components extends React.Component {
-    render() {
-        return (
-            <BaseLayout title="Build serverless websites">
-                <Websites />
-            </BaseLayout>
-        );
-    }
-}
+const ServerlessWebsites = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
 
-export default Components;
+    return (
+        <BaseLayout title="Build serverless websites">
+            <Websites />
+        </BaseLayout>
+    );
+};
+
+export default ServerlessWebsites;
