39d20ef9586951a7b5d1af543d21b993786bf9d7
Ashutosh <bhardwajashu96@hotmail.com>
2020-09-21 12:07:30 +0530

    feat: update `blog page` page
    add `Blogs` sections

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,62 +1,39 @@
-import React from 'react';
-import {graphql, Link} from 'gatsby';
+import React from "react";
+import { graphql } from "gatsby";
+import BaseLayout from "../layouts/base";
+import Blogs from "../components/blog/blog";
 
-import BaseLayout from '../layouts/base';
+export default ({ data }) => {
+    const blogPosts = data.allMarkdownRemark.nodes;
 
-const postCard = post => {
-  // handle image transformation exceptions
-  let featureImage = post.featureImage.publicURL;
-  if (post.featureImage.childImageSharp !== null) {
-    featureImage = post.featureImage.childImageSharp.fluid.src;
-  }
-
-  return (
-    <div key={post.slug}>
-      <img src={featureImage} />
-      <h3>
-        <Link to={'/' + post.slug}>{post.title}</Link>
-      </h3>
-      {post.description}<br />
-      {post.author}
-    </div>
-  );
-};
-
-export default data => {
-  const blogPosts = data.data.allMarkdownRemark.edges;
-
-  return (
-    <BaseLayout title="Webiny Serverless Blog">
-      <div>
-        {blogPosts.map (node => {
-          return postCard (node.node.frontmatter);
-        })}
-      </div>
-    </BaseLayout>
-  );
+    return (
+        <BaseLayout title="Webiny Serverless Blog">
+            <Blogs data={blogPosts} />
+        </BaseLayout>
+    );
 };
 
 export const listBlogsQuery = graphql`
-query MyQuery {
-    allMarkdownRemark (limit: 1000) {
-      edges {
-        node {
-          frontmatter {
-            author
-            description
-            slug
-            title
-            featureImage {
-              publicURL
-              childImageSharp {
-                fluid(maxWidth: 800) {
-                  ...GatsbyImageSharpFluid
+    query MyQuery {
+        allMarkdownRemark(limit: 1000) {
+            nodes {
+                id
+                frontmatter {
+                    author
+                    description
+                    slug
+                    title
+                    featureImage {
+                        name
+                        publicURL
+                        childImageSharp {
+                            fluid(maxWidth: 800) {
+                                ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-      }
     }
-  }
 `;
