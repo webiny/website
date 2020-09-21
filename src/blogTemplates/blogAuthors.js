2c456a759118b8ae85cf98f90933cf58d308e03d
SvenAlHamad <sven@webiny.com>
2020-09-21 11:31:37 +0100

    Finalized blog post author list.

--- src/blogTemplates/blogAuthors.js
+++ src/blogTemplates/blogAuthors.js
@@ -1,10 +1,52 @@
 import svenImg from './assets/sven.png';
+import pavelImg from './assets/pavel.png';
+import adrianImg from './assets/adrian.png';
+import ashuImg from './assets/ashu.png';
+import albionaImg from './assets/albiona.png';
+import mislavImg from './assets/mislav.png';
+import brunoImg from './assets/bruno.png';
 
 export default {
   sven: {
     name: 'Sven Al Hamad',
-    description: 'CEO & Co-Founder of Webiny',
-    twitter: '@SvenAlHamad',
+    description: 'CEO & Co-Founder @ Webiny',
+    twitter: 'SvenAlHamad',
     avatar: svenImg,
+  },
+  pavel: {
+    name: 'Pavel Denisjuk',
+    description: 'CTO & Co-Founder @ Webiny',
+    twitter: 'paveldenisjuk',
+    avatar: pavelImg,
+  },
+  adrian: {
+    name: 'Adrian Smijulj',
+    description: 'Lead Developer @ Webiny',
+    twitter: 'doitadrian',
+    avatar: adrianImg,
+  },
+  ashu: {
+    name: 'Ashutosh',
+    description: 'Full-stack Developer @ Webiny',
+    twitter: 'BhardwajAshu96',
+    avatar: ashuImg,
+  },
+  albiona: {
+    name: 'Albiona Hoti',
+    description: 'Developer Relations @ Webiny',
+    twitter: 'albionaitoh',
+    avatar: albionaImg,
+  },
+  mislav: {
+    name: 'Mislav Streicher',
+    description: 'Business Development @ Webiny',
+    twitter: false,
+    avatar: mislavImg,
+  },
+  bruno: {
+    name: 'Bruno Zoric',
+    description: 'Full-stack Developer @ Webiny',
+    twitter: false,
+    avatar: brunoImg,
   },
 };
