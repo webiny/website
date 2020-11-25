f1cb86ffd4cebde1abc5c9aa53991e4d6fb12236
albionahoti <albionahh@outlook.com>
2020-11-25 17:57:29 +0100

    update highlight code plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+// gatsby-browser.js
+require("prismjs/themes/prism.css");
 
 exports.onRouteUpdate = ({ location, prevLocation }) => {
     if (window.posthog) {
