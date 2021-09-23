3d59676349ba0e7125583585d0e51e165ec286a3
ashu96 <bhardwajashu96@hotmail.com>
2021-09-23 17:00:44 +0530

    fix: update FeaturedCard title size

--- src/components/knowledge-base/components/postCard.js
+++ src/components/knowledge-base/components/postCard.js
@@ -4,16 +4,27 @@ import React from "react";
 import styled from "react-emotion";
 // assets
 import { navigate } from "gatsby";
+import mq from "../../utils/breakpoints";
+import theme from "../../utils/theme";
 
-const PostCardWrapper = styled(FeaturedBlogWrapper)({
-    "& .text-container": {
-        "& .author-wrapper": {
-            "& .author__details": {
-                paddingLeft: 0,
+const PostCardWrapper = styled(FeaturedBlogWrapper)(
+    {
+        "& .text-container": {
+            "& .author-wrapper": {
+                "& .author__details": {
+                    paddingLeft: 0,
+                },
             },
         },
     },
-});
+    mq({
+        "& .text-container": {
+            "& .title": {
+                fontSize: [theme.fontSize["3xl"], "2.5rem"],
+            },
+        },
+    }),
+);
 
 export default ({ data }) => {
     // handle image transformation exceptions
