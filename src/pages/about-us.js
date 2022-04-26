4bb2b419620a20751a9f5a554db92d90ad2c4bdb
endymion1818 <endymion1818@gmail.com>
2022-04-26 14:30:41 +0100

    fix(about-us): update description

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -6,7 +6,7 @@ import Hero from "../components/about-us/about-us";
 class AboutUsPage extends React.Component {
     render() {
         return (
-            <BaseLayout title="About Us">
+            <BaseLayout title="About Us" description="At Webiny we have a mission to empower every developer to create serverless applications by providing tools, processes and ready-made apps.">
                 <Hero />
             </BaseLayout>
         );
