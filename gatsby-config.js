bcf73785ce089286277f7bb66a0771df42ea7014
endymion1818 <endymion1818@gmail.com>
2022-05-31 14:32:40 +0100

    fix(gatsbyconfig) rm gist embeds plugin - unused

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,23 +23,6 @@ module.exports = {
                 autoLabel: true,
             },
         },
-        // {
-        //     resolve: "gatsby-plugin-google-marketing-platform",
-        //     options: {
-        //         dataLayer: {
-        //             // Preset dataLayer values
-        //             gaPropertyId: "UA-35527198-1",
-        //         },
-        //         analytics: {
-        //             id: "UA-35527198-1",
-        //         },
-        //         optimize: {
-        //             id: "GTM-NJG8KQL",
-        //             timeout: 1000,
-        //             activateOn: "observer",
-        //         },
-        //     },
-        // },
         {
             resolve: "gatsby-plugin-facebook-pixel",
             options: {
@@ -107,9 +90,6 @@ module.exports = {
                         },
                     },
                     {
-                        resolve: "gatsby-remark-embed-gist",
-                    },
-                    {
                         resolve: "@weknow/gatsby-remark-twitter",
                     },
                     {
@@ -133,41 +113,11 @@ module.exports = {
                     {
                         resolve: `gatsby-remark-prismjs`,
                         options: {
-                            // Class prefix for <pre> tags containing syntax highlighting;
-                            // defaults to 'language-' (e.g. <pre class="language-js">).
-                            // If your site loads Prism into the browser at runtime,
-                            // (e.g. for use with libraries like react-live),
-                            // you may use this to prevent Prism from re-processing syntax.
-                            // This is an uncommon use-case though;
-                            // If you're unsure, it's best to use the default value.
                             classPrefix: "language-",
-                            // This is used to allow setting a language for inline code
-                            // (i.e. single backticks) by creating a separator.
-                            // This separator is a string and will do no white-space
-                            // stripping.
-                            // A suggested value for English speakers is the non-ascii
-                            // character '›'.
                             inlineCodeMarker: null,
-                            // This lets you set up language aliases.  For example,
-                            // setting this to '{ sh: "bash" }' will let you use
-                            // the language "sh" which will highlight using the
-                            // bash highlighter.
                             aliases: {},
-                            // This toggles the display of line numbers globally alongside the code.
-                            // To use it, add the following line in gatsby-browser.js
-                            // right after importing the prism color scheme:
-                            //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-                            // Defaults to false.
-                            // If you wish to only show line numbers on certain code blocks,
-                            // leave false and use the {numberLines: true} syntax below
                             showLineNumbers: false,
-                            // If setting this to true, the parser won't handle and highlight inline
-                            // code used in markdown i.e. single backtick code like `this`.
                             noInlineHighlight: false,
-                            // This adds a new language definition to Prism or extend an already
-                            // existing language definition. More details on this option can be
-                            // found under the header "Add new language definition or extend an
-                            // existing language" below.
                             languageExtensions: [
                                 {
                                     language: "superscript",
@@ -182,16 +132,11 @@ module.exports = {
                                     },
                                 },
                             ],
-                            // Customize the prompt used in shell output
-                            // Values below are default
                             prompt: {
                                 user: "root",
                                 host: "localhost",
                                 global: false,
                             },
-                            // By default the HTML entities <>&'" are escaped.
-                            // Add additional HTML escapes by providing a mapping
-                            // of HTML entities and their escape value IE: { '}': '&#123;' }
                             escapeEntities: {},
                         },
                     },
@@ -202,9 +147,6 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
-                    {
-                        resolve: "gatsby-remark-embed-gist",
-                    },
                     {
                         resolve: "@weknow/gatsby-remark-twitter",
                     },
@@ -222,25 +164,11 @@ module.exports = {
             resolve: "gatsby-plugin-google-tagmanager",
             options: {
                 id: "GTM-5TG2W9G",
-
-                // Include GTM in development.
-                //
-                // Defaults to false meaning GTM will only be loaded in production.
                 includeInDevelopment: false,
-
-                // datalayer to be set before GTM is loaded
-                // should be an object or a function that is executed in the browser
-                //
-                // Defaults to null
                 defaultDataLayer: { platform: "gatsby" },
-
-                // Specify optional GTM environment details.
                 gtmAuth: "q6mZlPddNJ_r_QSuUBx2aA",
                 gtmPreview: "env-1",
                 dataLayerName: "dataLayer",
-
-                // Name of the event that is triggered
-                // on every Gatsby route change.
             },
         },
     ],
