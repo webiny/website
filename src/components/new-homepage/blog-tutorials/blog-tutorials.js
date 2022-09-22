27b510a728a768fdd754b15338aa1496ed9d8a10
Ben Read <endymion1818@gmail.com>
2022-09-22 10:33:55 +0100

    feat(homepage): 6 articles, style tidy

--- src/components/new-homepage/blog-tutorials/blog-tutorials.js
+++ src/components/new-homepage/blog-tutorials/blog-tutorials.js
@@ -7,28 +7,29 @@ import { breakpoints } from "../../utils/breakpoints";
 import { H2 } from "../product-overview/product-overview.styled";
 
 const Background = styled.div`
-    padding-bottom: 3rem;
+    padding-bottom: 6rem;
     border-bottom: 1px solid #FAF0F3;
 
     > h2 {
         margin-left: auto;
         margin-right: auto;
-        margin-bottom: 3rem;
+        margin-bottom: 5rem;
     }
 `
 
 const BlogGrid = styled.div`
     @media (min-width: ${breakpoints[0]}px) {
-        display: flex;
-        flex-direction: row;
-        gap: 1fr;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        row-gap: 4rem;
     }
 `
 
 const BlogTutorials = () => {
     const { allMdx } = useStaticQuery(graphql`
         query BlogTutorialsQuery {
-            allMdx(filter: {fileAbsolutePath: {regex: "/blogPosts/"}, frontmatter: {tags: {in: "build projects"}}}, limit: 3, sort: {fields: frontmatter___date, order: DESC}) {
+            allMdx(filter: {fileAbsolutePath: {regex: "/blogPosts/"}, frontmatter: {tags: {in: "build projects"}}}, limit: 6, sort: {fields: frontmatter___date, order: DESC}) {
                 edges {
                     node {
                         id
@@ -56,7 +57,7 @@ const BlogTutorials = () => {
 
     return (
         <Background>
-            <H2 align="center">Learn what you can <span>build</span> with Webiny Headless CMS!</H2>
+            <H2 align="center">What can you <span>build</span> <br/>with Webiny Headless CMS?</H2>
             <BlogGrid>
                 {allMdx.edges.map(({ node }) => (
                     <BlogCard key={node.id} {...node.frontmatter} />
