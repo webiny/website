228c4d0e781733553e04bbd4585b20aac8a9bec0
Sven <sven@webiny.com>
2022-11-01 19:09:08 +0000

    Enterprise ToS (#207)
    * Enterprise ToS
    
    * docs: updated enterprise-agreement
    docs: updated privacy policy with Sopro disclaimer
    fix: fixed rendering of links - links now being displayed inline rather than new lines
    
    Co-authored-by: Pedro <plsalvado@gmail.com>

---
+++ src/pages/enterprise-agreement.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { MDXProvider } from "@mdx-js/react";
+import Policy from "./enterprise-agreement-standard.mdx";
+import BaseLayout from "../layouts/base";
+import StaticTemplateBase from "../components/static/static-base";
+
+class PrivacyPolicyPage extends React.Component {
+    render() {
+        return (
+            <BaseLayout
+                title="Webiny - Enterprise License Terms"
+                description="Webiny - Enterprise Agreement"
+            >
+                <MDXProvider>
+                    <StaticTemplateBase title={"Webiny Enterprise License Terms"}>
+                        <Policy />
+                    </StaticTemplateBase>
+                </MDXProvider>
+            </BaseLayout>
+        );
+    }
+}
+
+export default PrivacyPolicyPage;
